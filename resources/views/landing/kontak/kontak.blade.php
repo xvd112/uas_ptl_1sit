273f4f9c8c1cb958ba2af816f7f619be5100483e
Safira Putri Nabila <safira.putri@alpabit.com>
2024-02-03 10:52:59 +0700

    kontak lagi

--- resources/views/landing/kontak/kontak.blade.php
+++ resources/views/landing/kontak/kontak.blade.php
@@ -7,18 +7,36 @@
             <h5>Rumah Sakit Kasih Ibu</h5>
         </div>
         <section id="bedah" class="bedah" data-aos="fade-up">
-            <div class="container">
-                @forelse ($data as $d)
-                    <h5> {{ $d->email }}</h5>
-                    <h5> {{ $d->alamat }}</h5>
-                    <h5> {{ $d->telepon }}</h5>
-                    <h5> {{ $d->twitter }}</h5>
-                    <h5> {{ $d->fb }}</h5>
-                    <h5> {{ $d->ig }}</h5>
-                    <h5> {{ $d->linkin }}</h5>
-                @empty
-                    <p>Data tidak ditemukan !</p>
-                @endforelse
+            <h6>
+                <div class="container">
+                    @forelse ($data as $d)
+                        <div>
+                            <label class="col-sm-3">Email</label> : {{ $d->email }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">Alamat</label> : {{ $d->alamat }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">No Telp</label> : {{ $d->telepon }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">Twitter</label> : {{ $d->twitter }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">Facebook</label> : {{ $d->fb }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">Instagram</label> : {{ $d->ig }}
+                        </div>
+                        <div>
+                            <label class="col-sm-3">Linkin</label> : {{ $d->linkin }}
+                        </div>
+                    @empty
+                        <p>Data tidak ditemukan !</p>
+                    @endforelse
+
+                </div>
+            </h6>
         </section>
     </main>
 @endsection
